d322e10b40728a33e2a4480d4be1e35de816e7fe
Edinson Cabrera <cabreraed2607@gmail.com>
2026-08-08 03:09:04 -0500

    Initial Commit

---
+++ MxLite/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MxLite
+//
+//  Created by Edinson Cabrera on 8/08/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MxLite/MxLiteApp.swift
@@ -0,0 +1,17 @@
+//
+//  MxLiteApp.swift
+//  MxLite
+//
+//  Created by Edinson Cabrera on 8/08/26.
+//
+
+import SwiftUI
+
+@main
+struct MxLiteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
